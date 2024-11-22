bdd69c35166d3f69f410b0997b44ae4c100e81c4
Pulkiteleven <92856883+Pulkiteleven@users.noreply.github.com>
2024-11-22 16:21:04 +0530

    new test

--- server.js
+++ server.js
@@ -8,8 +8,10 @@ const port = process.env.PORT || 2000;
 
 app.use(express.json());
 
-
-app.use("/api",require("./router/router"))
+app.get('/home', (req, res) => {
+    res.status(200).json('Welcome, your app is working well');
+  })
+app.use("/api",require("./router/router"));
 
 app.use(errorHandler);
 
